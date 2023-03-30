eb97e1ade417ee0d9b11e26a9658ca44f0d3eef9
midlajt <muhammedmidlaj236@gmail.com>
2023-03-30 12:39:21 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontEndComponent } from './front-end/front-end.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontEndComponent
   ],
   imports: [
     BrowserModule,
