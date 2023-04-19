bdcc2fa640b423b2258207851cf3ecfe260a0293
midlajt <muhammedmidlaj236@gmail.com>
2023-04-19 21:14:28 +0530

    product_page-udpate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontEndComponent } from './front-end/front-end.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:FrontEndComponent},
+  {path:"product-page",component:ProductPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontEndComponent } from './front-end/front-end.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontEndComponent
+    FrontEndComponent,
+    ProductPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
